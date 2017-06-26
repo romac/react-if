565e5d9872324a4fa948353539deeaed0b7a09fe
Andrej Mihajlov <and@codeispoetry.ru>
2017-06-26 18:30:38 +0300

    Migrate to React 15.5+

--- src/ReactIf.js
+++ src/ReactIf.js
@@ -1,5 +1,6 @@
 
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
 function render(props) {
   if (typeof props.children === 'function') {
