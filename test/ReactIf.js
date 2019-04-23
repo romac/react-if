e5402a856dd8da2f0088764a24ba74a2491c845e
Cyril CHAPON <cyril@permettezmoideconstruire.fr>
2019-04-23 17:32:39 +0200

    Add lazy condition evalutation

--- test/ReactIf.js
+++ test/ReactIf.js
@@ -88,7 +88,7 @@ describe("react-if", function(){
 				expect(wrapper.containsMatchingElement(<div>Else</div>)).to.equal(false);
 			});
 		});
-		
+
 		context("content without <Then /> or <Else />", function(){
 			it("should render the child", function(){
 				const wrapper = mount(
@@ -103,7 +103,87 @@ describe("react-if", function(){
 		context("without blocks", function(){
 			it("should render nothing", function(){
 				const wrapper = mount(<If condition={true}></If>);
-				
+
+				expect(wrapper.html()).to.equal(null);
+			});
+		});
+  });
+
+  context("<If /> element with true condition as a function", function(){
+		it("should render the child element of <Then />", function(){
+			const wrapper = mount(
+				<If condition={() => true}>
+					<Then>
+						<div>Then</div>
+					</Then>
+				</If>
+			);
+			expect(wrapper.containsMatchingElement(<div>Then</div>)).to.equal(true);
+		});
+
+		it("should not render the child element of <Else />", function(){
+			const wrapper = mount(
+				<If condition={() => true}>
+					<Then>
+						<div>Then</div>
+					</Then>
+					<Else>
+						<div>Else</div>
+					</Else>
+				</If>
+			);
+			expect(wrapper.containsMatchingElement(<div>Else</div>)).to.equal(false);
+		});
+
+		context("multiple <Then /> blocks", function(){
+			it("should only render the first <Then /> block", function(){
+				const wrapper = mount(
+					<If condition={() => true}>
+						<Then>
+							<div>Then1</div>
+						</Then>
+						<Then>
+							<div>Then2</div>
+						</Then>
+					</If>
+				);
+				expect(wrapper.containsMatchingElement(<div>Then1</div>)).to.equal(true);
+				expect(wrapper.containsMatchingElement(<div>Then2</div>)).to.equal(false);
+			});
+		});
+
+		context("<Else /> before <Then />", function(){
+			it("should only render the <Then /> block", function(){
+				const wrapper = mount(
+					<If condition={() => true}>
+						<Else>
+							<div>Else</div>
+						</Else>
+						<Then>
+							<div>Then</div>
+						</Then>
+					</If>
+				);
+				expect(wrapper.containsMatchingElement(<div>Then</div>)).to.equal(true);
+				expect(wrapper.containsMatchingElement(<div>Else</div>)).to.equal(false);
+			});
+		});
+
+		context("content without <Then /> or <Else />", function(){
+			it("should render the child", function(){
+				const wrapper = mount(
+					<If condition={() => true}>
+						<div>Content</div>
+					</If>
+				);
+				expect(wrapper.containsMatchingElement(<div>Content</div>)).to.equal(true);
+			});
+		});
+
+		context("without blocks", function(){
+			it("should render nothing", function(){
+				const wrapper = mount(<If condition={() => true}></If>);
+
 				expect(wrapper.html()).to.equal(null);
 			});
 		});
@@ -168,7 +248,7 @@ describe("react-if", function(){
 				expect(wrapper.containsMatchingElement(<div>Else</div>)).to.equal(true);
 			});
 		});
-		
+
 		context("content without <Then /> or <Else />", function(){
 			it("should not render the child", function(){
 				const wrapper = mount(
@@ -183,7 +263,7 @@ describe("react-if", function(){
 		context("without blocks", function(){
 			it("should render nothing", function(){
 				const wrapper = mount(<If condition={false}></If>);
-				
+
 				expect(wrapper.html()).to.equal(null);
 			});
 		});
@@ -219,6 +299,118 @@ describe("react-if", function(){
 				expect(called).to.equal(false);
 			});
 		});
+  });
+
+	context("<If /> element with false condition as a function", function(){
+		it("should not render the child element of <Then />", function(){
+			const wrapper = mount(
+				<If condition={() => false}>
+					<Then>
+						<div>Then</div>
+					</Then>
+				</If>
+			);
+			expect(wrapper.containsMatchingElement(<div>Then</div>)).to.equal(false);
+		});
+
+		it("should render the child element of <Else />", function(){
+			const wrapper = mount(
+				<If condition={() => false}>
+					<Then>
+						<div>Then</div>
+					</Then>
+					<Else>
+						<div>Else</div>
+					</Else>
+				</If>
+			);
+			expect(wrapper.containsMatchingElement(<div>Else</div>)).to.equal(true);
+		});
+
+		context("multiple <Else /> blocks", function(){
+			it("should only render the first <Else /> block", function(){
+				const wrapper = mount(
+					<If condition={() => false}>
+						<Else>
+							<div>Else1</div>
+						</Else>
+						<Else>
+							<div>Else2</div>
+						</Else>
+					</If>
+				);
+				expect(wrapper.containsMatchingElement(<div>Else1</div>)).to.equal(true);
+				expect(wrapper.containsMatchingElement(<div>Else2</div>)).to.equal(false);
+			});
+		});
+
+		context("<Else /> before <Then />", function(){
+			it("should only render the <Else /> block", function(){
+				const wrapper = mount(
+					<If condition={() => false}>
+						<Else>
+							<div>Else</div>
+						</Else>
+						<Then>
+							<div>Then</div>
+						</Then>
+					</If>
+				);
+				expect(wrapper.containsMatchingElement(<div>Then</div>)).to.equal(false);
+				expect(wrapper.containsMatchingElement(<div>Else</div>)).to.equal(true);
+			});
+		});
+
+		context("content without <Then /> or <Else />", function(){
+			it("should not render the child", function(){
+				const wrapper = mount(
+					<If condition={() => false}>
+						<div>Content</div>
+					</If>
+				);
+				expect(wrapper.containsMatchingElement(<div>Content</div>)).to.equal(false);
+			});
+		});
+
+		context("without blocks", function(){
+			it("should render nothing", function(){
+				const wrapper = mount(<If condition={() => false}></If>);
+
+				expect(wrapper.html()).to.equal(null);
+			});
+		});
+
+		context("where a child is a function", function(){
+			it("should render the component returned by the function", function(){
+				const wrapper = mount(
+					<If condition={() => false}>
+						<Else>
+							{ () => <div>Else</div> }
+						</Else>
+					</If>
+				);
+				expect(wrapper.containsMatchingElement(<div>Else</div>)).to.equal(true);
+			});
+
+			it("should not evaluate the body of the function if not needed", function() {
+				var called = false;
+
+				const wrapper = mount(
+					<If condition={() => false}>
+						<Then>{() => {
+							called = true;
+							<div>Bad</div>
+						}}</Then>
+						<Else>
+							<div>Ok</div>
+						</Else>
+					</If>
+				);
+				expect(wrapper.containsMatchingElement(<div>Ok</div>)).to.equal(true);
+				expect(wrapper.containsMatchingElement(<div>Bad</div>)).to.equal(false);
+				expect(called).to.equal(false);
+			});
+		});
 	});
 
 	context("<When /> element", function(){
@@ -238,10 +430,33 @@ describe("react-if", function(){
 				</When>
 			);
 			expect(wrapper.containsMatchingElement(<div>When</div>)).to.equal(false);
-		});
-		
+    });
+
 		it("should render nothing", function(){
 			const wrapper = mount(<When condition={true}></When>);
+			expect(wrapper.html()).to.equal(null);
+		});
+
+		it("should render the child element of <When /> with condition as a function", function(){
+			const wrapper = mount(
+				<When condition={() => true}>
+					<div>When</div>
+				</When>
+			);
+			expect(wrapper.containsMatchingElement(<div>When</div>)).to.equal(true);
+		});
+
+		it("should not render the child element of <When /> with condition as a function", function(){
+			const wrapper = mount(
+				<When condition={() => false}>
+					<div>When</div>
+				</When>
+			);
+			expect(wrapper.containsMatchingElement(<div>When</div>)).to.equal(false);
+		});
+
+		it("should render nothing with condition as a function", function(){
+			const wrapper = mount(<When condition={() => true}></When>);
 			expect(wrapper.html()).to.equal(null);
 		});
 	});
@@ -264,9 +479,32 @@ describe("react-if", function(){
 			);
 			expect(wrapper.containsMatchingElement(<div>Unless</div>)).to.equal(true);
 		});
-		
+
 		it("should render nothing", function(){
 			const wrapper = mount(<Unless condition={true}></Unless>);
+			expect(wrapper.html()).to.equal(null);
+    });
+
+		it("should not render the child element of <Unless />", function(){
+			const wrapper = mount(
+				<Unless condition={() => true}>
+					<div>Unless</div>
+				</Unless>
+			);
+			expect(wrapper.containsMatchingElement(<div>Unless</div>)).to.equal(false);
+		});
+
+		it("should render the child element of <Unless />", function(){
+			const wrapper = mount(
+				<Unless condition={() => false}>
+					<div>Unless</div>
+				</Unless>
+			);
+			expect(wrapper.containsMatchingElement(<div>Unless</div>)).to.equal(true);
+		});
+
+		it("should render nothing", function(){
+			const wrapper = mount(<Unless condition={() => true}></Unless>);
 			expect(wrapper.html()).to.equal(null);
 		});
 	});
