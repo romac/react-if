54e3638cbad42e68f3ddbe0e1e033ce05bdc8aa0
Cyril CHAPON <cyril@permettezmoideconstruire.fr>
2019-04-24 11:50:47 +0200

    Add Switch/Case statement

--- src/ReactIf.js
+++ src/ReactIf.js
@@ -86,6 +86,82 @@ When.defaultProps = {
   children: null
 };
 
+export function Case(props) {
+  return render(props);
+}
+
+Case.propTypes = {
+  condition: PropTypes.oneOfType([PropTypes.func, PropTypes.bool]).isRequired,
+  children: PropTypes.oneOfType([PropTypes.func, PropTypes.node])
+};
+
+Case.defaultProps = {
+  children: null
+};
+
+export function Default(props) {
+  return render(props);
+}
+
+Default.propTypes = {
+  children: PropTypes.oneOfType([PropTypes.func, PropTypes.node])
+};
+
+Default.defaultProps = {
+  children: null
+};
+
+export function Switch({ children }) {
+  // -- Inspired from react-router --
+
+  // We use React.Children.forEach instead of React.Children.toArray().find()
+  // here because toArray adds keys to all child elements and we do not want
+  // to trigger an unmount/remount for two children <Case>s or <Default>s
+  let matchingCase;
+  let defaultCase;
+
+  React.Children.forEach(children, child => {
+    // not a valid react child, don't add it
+    if (!React.isValidElement(child)) {
+      return
+    }
+
+    if (
+      !matchingCase &&
+      child.type === Case
+    ) {
+      const condition = child.props.condition
+
+      const conditionResult = getConditionResult(condition)
+
+      if (conditionResult) {
+        matchingCase = child
+      } // else not matching condition, don't add it
+    } else if (
+      !defaultCase &&
+      child.type === Default
+    ) {
+      defaultCase = child
+    }// else unknown type, don't add it
+  });
+
+  return matchingCase || defaultCase || null
+}
+
+const CaseOrDefault = PropTypes.oneOfType([
+  PropTypes.instanceOf(Case),
+  PropTypes.instanceOf(Default),
+  PropTypes.node
+]);
+
+Switch.propTypes = {
+  children: PropTypes.oneOfType([PropTypes.arrayOf(CaseOrDefault), CaseOrDefault])
+};
+
+Switch.defaultProps = {
+  children: null
+};
+
 If.Then = Then;
 If.Else = Else;
 If.When = When;
--- test/ReactIf.js
+++ test/ReactIf.js
@@ -3,7 +3,7 @@ import { expect } from 'chai';
 import { configure, mount } from 'enzyme';
 import { JSDOM } from 'jsdom';
 import Adapter from 'enzyme-adapter-react-16';
-import { If, Then, Else, When, Unless } from '../src/ReactIf';
+import { If, Then, Else, When, Unless, Switch, Case, Default } from '../src/ReactIf';
 
 function setUpDomEnvironment() {
 	const dom = new JSDOM('<!doctype html><html><body></body></html>', {url: 'http://localhost/'});
@@ -507,6 +507,312 @@ describe("react-if", function(){
 			const wrapper = mount(<Unless condition={() => true}></Unless>);
 			expect(wrapper.html()).to.equal(null);
 		});
-	});
+  });
 
+	context("<Switch /> element", function(){
+		it("should render the child element of <Case />", function(){
+			const wrapper = mount(
+				<Switch>
+					<Case condition={true}>
+						<div>Case</div>
+					</Case>
+					<Case condition={true}>
+						<div>Case</div>
+					</Case>
+				</Switch>
+      );
+			expect(wrapper.containsMatchingElement(<div>Case</div>)).to.equal(true);
+    });
+
+		it("should render the child element of <Case /> (with condition as a function)", function(){
+			const wrapper = mount(
+				<Switch>
+					<Case condition={() => true}>
+						<div>Case</div>
+					</Case>
+				</Switch>
+			);
+			expect(wrapper.containsMatchingElement(<div>Case</div>)).to.equal(true);
+    });
+
+		it("should render the child element of <Case /> (with children render function)", function(){
+			const wrapper = mount(
+				<Switch>
+					<Case condition={true}>
+						{() => <div>Case</div>}
+					</Case>
+				</Switch>
+			);
+			expect(wrapper.containsMatchingElement(<div>Case</div>)).to.equal(true);
+    });
+
+		it("should render the child element of <Default />", function(){
+			const wrapper = mount(
+				<Switch>
+					<Default>
+						<div>Case</div>
+					</Default>
+				</Switch>
+			);
+			expect(wrapper.containsMatchingElement(<div>Case</div>)).to.equal(true);
+    });
+
+		it("should render the child element of <Default /> (with children render function)", function(){
+			const wrapper = mount(
+				<Switch>
+					<Default>
+						{() => <div>Case</div>}
+					</Default>
+				</Switch>
+			);
+			expect(wrapper.containsMatchingElement(<div>Case</div>)).to.equal(true);
+    });
+
+		it("should render nothing", function(){
+			const wrapper = mount(
+        <Switch />
+      );
+			expect(wrapper.html()).to.equal(null);
+    });
+
+    it("should render nothing with not matching case", function(){
+      const wrapper = mount(
+        <Switch>
+          <Case condition={false}>
+            <div>Case</div>
+          </Case>
+        </Switch>
+      );
+      expect(wrapper.html()).to.equal(null);
+    });
+
+    context("multiple <Case /> blocks", function(){
+      it("should only render the first <Case /> block", function(){
+        const wrapper = mount(
+          <Switch>
+            <Case condition={true}>
+              <div>FirstCase</div>
+            </Case>
+            <Case condition={true}>
+              <div>SecondCase</div>
+            </Case>
+          </Switch>
+        );
+        expect(wrapper.containsMatchingElement(<div>FirstCase</div>)).to.equal(true);
+        expect(wrapper.containsMatchingElement(<div>SecondCase</div>)).to.equal(false);
+      });
+
+      it("should only render the second <Case /> block", function(){
+        const wrapper = mount(
+          <Switch>
+            <Case condition={false}>
+              <div>FirstCase</div>
+            </Case>
+            <Case condition={true}>
+              <div>SecondCase</div>
+            </Case>
+          </Switch>
+        );
+        expect(wrapper.containsMatchingElement(<div>FirstCase</div>)).to.equal(false);
+        expect(wrapper.containsMatchingElement(<div>SecondCase</div>)).to.equal(true);
+      });
+
+      it("should render the second <Case /> block (direct condition + condition as a function)", function(){
+        const wrapper = mount(
+          <Switch>
+            <Case condition={false}>
+              <div>FirstCase</div>
+            </Case>
+            <Case condition={() => true}>
+              <div>SecondCase</div>
+            </Case>
+          </Switch>
+        );
+        expect(wrapper.containsMatchingElement(<div>FirstCase</div>)).to.equal(false);
+        expect(wrapper.containsMatchingElement(<div>SecondCase</div>)).to.equal(true);
+      });
+    })
+
+    context("multiple <Default /> blocks", function(){
+      it("should only render the first <Default /> block", function(){
+        const wrapper = mount(
+          <Switch>
+            <Default>
+              <div>FirstCase</div>
+            </Default>
+            <Default>
+              <div>SecondCase</div>
+            </Default>
+          </Switch>
+        );
+        expect(wrapper.containsMatchingElement(<div>FirstCase</div>)).to.equal(true);
+        expect(wrapper.containsMatchingElement(<div>SecondCase</div>)).to.equal(false);
+      });
+    })
+
+    context("<Case /> + <Default /> blocks", function(){
+      it("should only render the <Case /> block", function(){
+        const wrapper = mount(
+          <Switch>
+            <Case condition={true}>
+              <div>FirstCase</div>
+            </Case>
+            <Default>
+              <div>DefaultCase</div>
+            </Default>
+          </Switch>
+        );
+        expect(wrapper.containsMatchingElement(<div>FirstCase</div>)).to.equal(true);
+        expect(wrapper.containsMatchingElement(<div>DefaultCase</div>)).to.equal(false);
+      });
+
+      it("should only render the <Case /> block (condition as a function)", function(){
+        const wrapper = mount(
+          <Switch>
+            <Case condition={() => true}>
+              <div>FirstCase</div>
+            </Case>
+            <Default>
+              <div>DefaultCase</div>
+            </Default>
+          </Switch>
+        );
+        expect(wrapper.containsMatchingElement(<div>FirstCase</div>)).to.equal(true);
+        expect(wrapper.containsMatchingElement(<div>DefaultCase</div>)).to.equal(false);
+      });
+
+      it("should only render the <Default /> block", function(){
+        const wrapper = mount(
+          <Switch>
+            <Case condition={false}>
+              <div>FirstCase</div>
+            </Case>
+            <Default>
+              <div>DefaultCase</div>
+            </Default>
+          </Switch>
+        );
+        expect(wrapper.containsMatchingElement(<div>FirstCase</div>)).to.equal(false);
+        expect(wrapper.containsMatchingElement(<div>DefaultCase</div>)).to.equal(true);
+      });
+    })
+
+    context("<Default /> before <Case /> blocks", function(){
+      it("should only render the <Case /> block", function(){
+        const wrapper = mount(
+          <Switch>
+            <Default>
+              <div>DefaultCase</div>
+            </Default>
+            <Case condition={true}>
+              <div>FirstCase</div>
+            </Case>
+          </Switch>
+        );
+        expect(wrapper.containsMatchingElement(<div>FirstCase</div>)).to.equal(true);
+        expect(wrapper.containsMatchingElement(<div>DefaultCase</div>)).to.equal(false);
+      });
+
+      it("should only render the <Case /> block (condition as a function)", function(){
+        const wrapper = mount(
+          <Switch>
+            <Default>
+              <div>DefaultCase</div>
+            </Default>
+            <Case condition={() => true}>
+              <div>FirstCase</div>
+            </Case>
+          </Switch>
+        );
+        expect(wrapper.containsMatchingElement(<div>FirstCase</div>)).to.equal(true);
+        expect(wrapper.containsMatchingElement(<div>DefaultCase</div>)).to.equal(false);
+      });
+
+      it("should only render the <Default /> block", function(){
+        const wrapper = mount(
+          <Switch>
+            <Default>
+              <div>DefaultCase</div>
+            </Default>
+            <Case condition={false}>
+              <div>FirstCase</div>
+            </Case>
+          </Switch>
+        );
+        expect(wrapper.containsMatchingElement(<div>FirstCase</div>)).to.equal(false);
+        expect(wrapper.containsMatchingElement(<div>DefaultCase</div>)).to.equal(true);
+      });
+    })
+
+    context("Multiple <Case /> and multiple <Default /> blocks", function(){
+      it("should only render the first <Case /> block", function(){
+        const wrapper = mount(
+          <Switch>
+            <Default>
+              <div>FirstDefaultCase</div>
+            </Default>
+            <Case condition={true}>
+              <div>FirstCase</div>
+            </Case>
+            <Case condition={false}>
+              <div>SecondCase</div>
+            </Case>
+            <Default>
+              <div>SecondDefaultCase</div>
+            </Default>
+          </Switch>
+        );
+        expect(wrapper.containsMatchingElement(<div>FirstCase</div>)).to.equal(true);
+        expect(wrapper.containsMatchingElement(<div>SecondCase</div>)).to.equal(false);
+        expect(wrapper.containsMatchingElement(<div>FirstDefaultCase</div>)).to.equal(false);
+        expect(wrapper.containsMatchingElement(<div>SecondDefaultCase</div>)).to.equal(false);
+      });
+
+      it("should only render the second <Case /> block", function(){
+        const wrapper = mount(
+          <Switch>
+            <Default>
+              <div>FirstDefaultCase</div>
+            </Default>
+            <Case condition={false}>
+              <div>FirstCase</div>
+            </Case>
+            <Case condition={true}>
+              <div>SecondCase</div>
+            </Case>
+            <Default>
+              <div>SecondDefaultCase</div>
+            </Default>
+          </Switch>
+        );
+        expect(wrapper.containsMatchingElement(<div>FirstCase</div>)).to.equal(false);
+        expect(wrapper.containsMatchingElement(<div>SecondCase</div>)).to.equal(true);
+        expect(wrapper.containsMatchingElement(<div>FirstDefaultCase</div>)).to.equal(false);
+        expect(wrapper.containsMatchingElement(<div>SecondDefaultCase</div>)).to.equal(false);
+      });
+
+      it("should only render the first <Case /> block", function(){
+        const wrapper = mount(
+          <Switch>
+            <Default>
+              <div>FirstDefaultCase</div>
+            </Default>
+            <Case condition={false}>
+              <div>FirstCase</div>
+            </Case>
+            <Case condition={false}>
+              <div>SecondCase</div>
+            </Case>
+            <Default>
+              <div>SecondDefaultCase</div>
+            </Default>
+          </Switch>
+        );
+        expect(wrapper.containsMatchingElement(<div>FirstCase</div>)).to.equal(false);
+        expect(wrapper.containsMatchingElement(<div>SecondCase</div>)).to.equal(false);
+        expect(wrapper.containsMatchingElement(<div>FirstDefaultCase</div>)).to.equal(true);
+        expect(wrapper.containsMatchingElement(<div>SecondDefaultCase</div>)).to.equal(false);
+      });
+    })
+  });
 });
