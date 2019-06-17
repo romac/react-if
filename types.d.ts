7eeb2df7596bae001fd92fc79806333281b9ca4c
Dmitry Melnik <dmitrymelnik96@gmail.com>
2019-06-17 11:35:09 +0300

    Type definition for Switch, Case and Default

--- types.d.ts
+++ types.d.ts
@@ -21,4 +21,17 @@ declare module 'react-if'{
 
   /** A shorthand for <If condition={...}><Else>...</Else></If>. The same rules apply to the child elements as with using the Else block. */
   export class Unless extends React.Component<IfOptions, any>{}
+
+  /**
+   * It will render the first matching <Case />, or the first encountered <Default /> (or null).
+   *
+   * This component can contain any number of <Case /> and one <Default /> blocks
+   */
+  export class Switch extends React.Component {}
+
+  /** If the <Case /> is the first one to have its condition evaluates to true inside the parent <Switch /> it will be the only rendered. */
+  export class Case extends React.Component<IfOptions, any> {}
+
+  /** If no <Case /> have its condition evaluates to true inside the parent <Switch />, the first <Default /> will be the only one rendered. */
+  export class Default extends React.Component {}
 }
