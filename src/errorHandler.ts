b279e25cceb2e4da5173780d83d74f7581ae8ec2
Jeroen Claassens <support@favware.tech>
2020-10-20 21:39:33 +0200

    refactor(if): conditionally check if error should be thrown
    If NODE_ENV is 'production' then the first part of the evaluation short-circuits the conditional
    passed into handleError, ensuring that in production environments the check for whether the error
    has to be thrown at all is not resolved, ultimately increasing performance. Furthermore if NODE_ENV
    is undefined or an empty string it is also assumed to be production, to provide for those who do not
    use a common framework such as create-react-app, NextJS or Gatsby.

--- src/errorHandler.ts
+++ src/errorHandler.ts
@@ -1,5 +1,8 @@
-export const handleError = (errorMessage: string) => {
-  if (process.env.NODE_ENV !== 'production') {
+/**
+ * Handles errors by throwing errors, only when `NODE_ENV` is not `'production'`
+ */
+export const handleError = (errorMessage: string, errorCondition: boolean) => {
+  if (process.env.NODE_ENV !== 'production' && errorCondition) {
     throw new Error(errorMessage);
   }
 };
