8860e0a9b1c922689204579a6f6257b831661bf2
Jeroen Claassens <support@favware.tech>
2020-10-20 22:26:15 +0200

    test: convert more tests

--- test/If.test.tsx
+++ test/If.test.tsx
@@ -1,128 +1,322 @@
-import { shallow } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import React from 'react';
 import { Else, If, Then } from '../src';
 
-describe('If component - with NODE_ENV === test', () => {
-  test('GIVEN If w/ <Then /> & truthy condition THEN renders children', () => {
-    const wrapped = shallow(
-      <If condition={true}>
-        <Then>
-          <span>Then</span>
-        </Then>
-      </If>
-    );
+describe('Truthy cases', () => {
+  describe('With NODE_ENV === test', () => {
+    test('GIVEN <Then /> THEN renders children', () => {
+      const wrapped = shallow(
+        <If condition={true}>
+          <Then>
+            <span>Then</span>
+          </Then>
+        </If>
+      );
 
-    expect(wrapped).toMatchSnapshot();
-    expect(wrapped.containsMatchingElement(<span>Then</span>)).toBe(true);
+      expect(wrapped).toMatchSnapshot();
+      expect(wrapped.containsMatchingElement(<span>Then</span>)).toBe(true);
+    });
+
+    test('GIVEN <Then /> & <Else /> THEN renders children of <Then />', () => {
+      const wrapped = shallow(
+        <If condition={true}>
+          <Then>
+            <span>Then</span>
+          </Then>
+          <Else>
+            <span>Else</span>
+          </Else>
+        </If>
+      );
+
+      expect(wrapped).toMatchSnapshot();
+      expect(wrapped.containsMatchingElement(<span>Then</span>)).toBe(true);
+      expect(wrapped.containsMatchingElement(<span>Else</span>)).toBe(false);
+    });
+
+    test('GIVEN multiple <Then /> blocks THEN renders only first <Then /> block', () => {
+      const wrapped = shallow(
+        <If condition={true}>
+          <Then>
+            <span>Then1</span>
+          </Then>
+          <Then>
+            <span>Then2</span>
+          </Then>
+        </If>
+      );
+
+      expect(wrapped).toMatchSnapshot();
+      expect(wrapped.containsMatchingElement(<span>Then1</span>)).toBe(true);
+      expect(wrapped.containsMatchingElement(<span>Then2</span>)).toBe(false);
+    });
+
+    test('GIVEN <Else /> before <Then /> THEN renders <Then /> block', () => {
+      const wrapped = shallow(
+        <If condition={true}>
+          <Else>
+            <span>Else</span>
+          </Else>
+          <Then>
+            <span>Then</span>
+          </Then>
+        </If>
+      );
+
+      expect(wrapped).toMatchSnapshot();
+      expect(wrapped.containsMatchingElement(<span>Then</span>)).toBe(true);
+      expect(wrapped.containsMatchingElement(<span>Else</span>)).toBe(false);
+    });
+
+    test('GIVEN content w/o <Then /> nor <Else /> THEN errors in test & development', () => {
+      expect(() =>
+        shallow(
+          <If condition={true}>
+            <span>Not Then nor Else</span>
+          </If>
+        )
+      ).toThrowError('The <If> component should contain <Then /> and <Else /> components as its children');
+    });
+
+    test('GIVEN w/o children THEN renders null', () => {
+      const wrapped = shallow(<If condition={true} />);
+
+      expect(wrapped).toMatchSnapshot();
+      expect(wrapped.html()).toBeNull();
+    });
   });
 
-  test('GIVEN If w/ <Then /> & <Else /> & truthy condition THEN renders children of <Then />', () => {
-    const wrapped = shallow(
-      <If condition={true}>
-        <Then>
-          <span>Then</span>
-        </Then>
-        <Else>
-          <span>Else</span>
-        </Else>
-      </If>
-    );
+  describe('With NODE_ENV === production', () => {
+    beforeEach(() => {
+      // @ts-expect-error __DEV__ is exposed by Jest
+      global.__DEV__ = false;
+    });
 
-    expect(wrapped).toMatchSnapshot();
-    expect(wrapped.containsMatchingElement(<span>Then</span>)).toBe(true);
-    expect(wrapped.containsMatchingElement(<span>Else</span>)).toBe(false);
-  });
+    afterAll(() => {
+      // @ts-expect-error __DEV__ is exposed by Jest
+      global.__DEV__ = true;
+    });
 
-  test('GIVEN If w/ <Then /> & <Else /> & falsy condition THEN renders children of <Else />', () => {
-    const wrapped = shallow(
-      <If condition={false}>
-        <Then>
-          <span>Then</span>
-        </Then>
-        <Else>
-          <span>Else</span>
-        </Else>
-      </If>
-    );
-
-    expect(wrapped).toMatchSnapshot();
-    expect(wrapped.containsMatchingElement(<span>Then</span>)).toBe(false);
-    expect(wrapped.containsMatchingElement(<span>Else</span>)).toBe(true);
-  });
-
-  test('GIVEN If w/ multiple <Then /> blocks THEN renders only first <Then /> block', () => {
-    const wrapped = shallow(
-      <If condition={true}>
-        <Then>
-          <span>Then1</span>
-        </Then>
-        <Then>
-          <span>Then2</span>
-        </Then>
-      </If>
-    );
-
-    expect(wrapped).toMatchSnapshot();
-    expect(wrapped.containsMatchingElement(<span>Then1</span>)).toBe(true);
-    expect(wrapped.containsMatchingElement(<span>Then2</span>)).toBe(false);
-  });
-
-  test('GIVEN If w/ <Else /> before <Then /> THEN renders <Then /> block', () => {
-    const wrapped = shallow(
-      <If condition={true}>
-        <Else>
-          <span>Else</span>
-        </Else>
-        <Then>
-          <span>Then</span>
-        </Then>
-      </If>
-    );
-
-    expect(wrapped).toMatchSnapshot();
-    expect(wrapped.containsMatchingElement(<span>Then</span>)).toBe(true);
-    expect(wrapped.containsMatchingElement(<span>Else</span>)).toBe(false);
-  });
-
-  test('GIVEN content w/o <Then /> nor <Else /> THEN errors in test & development', () => {
-    expect(() =>
-      shallow(
+    test('GIVEN content w/o <Then /> nor <Else /> THEN renders empty block in production', () => {
+      const wrapped = shallow(
         <If condition={true}>
           <span>Not Then nor Else</span>
         </If>
-      )
-    ).toThrowError('The <If> component should contain <Then /> and <Else /> components as its children');
+      );
+
+      expect(wrapped).toMatchSnapshot();
+      expect(wrapped.containsMatchingElement(<span>Not Then nor Else</span>)).toBe(true);
+    });
   });
 
-  test('GIVEN w/o children THEN renders null', () => {
-    const wrapped = shallow(<If condition={true} />);
+  describe('With condition as a function', () => {
+    test('GIVEN <Then /> THEN renders children of <Then />', () => {
+      const wrapped = shallow(
+        <If condition={() => true}>
+          <Then>
+            <span>Then</span>
+          </Then>
+        </If>
+      );
 
-    expect(wrapped).toMatchSnapshot();
-    expect(wrapped.html()).toBeNull();
+      expect(wrapped).toMatchSnapshot();
+      expect(wrapped.containsMatchingElement(<span>Then</span>)).toBe(true);
+    });
+
+    test('GIVEN <Then /> & <Else /> THEN does not render children of <Else />', () => {
+      const wrapped = shallow(
+        <If condition={() => true}>
+          <Then>
+            <span>Then</span>
+          </Then>
+          <Else>
+            <span>Else</span>
+          </Else>
+        </If>
+      );
+
+      expect(wrapped).toMatchSnapshot();
+      expect(wrapped.containsMatchingElement(<span>Then</span>)).toBe(true);
+      expect(wrapped.containsMatchingElement(<span>Else</span>)).toBe(false);
+    });
+
+    test('GIVEN multiple <Then /> blocks THEN renders only first <Then /> block', () => {
+      const wrapped = shallow(
+        <If condition={() => true}>
+          <Then>
+            <span>Then1</span>
+          </Then>
+          <Then>
+            <span>Then2</span>
+          </Then>
+        </If>
+      );
+
+      expect(wrapped).toMatchSnapshot();
+      expect(wrapped.containsMatchingElement(<span>Then1</span>)).toBe(true);
+      expect(wrapped.containsMatchingElement(<span>Then2</span>)).toBe(false);
+    });
+
+    test('GIVEN <Else /> before <Then /> THEN renders <Then /> block', () => {
+      const wrapped = shallow(
+        <If condition={() => true}>
+          <Else>
+            <span>Else</span>
+          </Else>
+          <Then>
+            <span>Then</span>
+          </Then>
+        </If>
+      );
+
+      expect(wrapped).toMatchSnapshot();
+      expect(wrapped.containsMatchingElement(<span>Then</span>)).toBe(true);
+      expect(wrapped.containsMatchingElement(<span>Else</span>)).toBe(false);
+    });
   });
 });
 
-describe('If Component - with NODE_ENV === production', () => {
-  const PRE_TEST_ENV = process.env;
+describe('Falsy condition', () => {
+  describe('With NODE_ENV === test', () => {
+    test('GIVEN <Then /> THEN renders null', () => {
+      const wrapped = shallow(
+        <If condition={false}>
+          <Then>
+            <span>Then</span>
+          </Then>
+        </If>
+      );
 
-  beforeEach(() => {
-    jest.resetModules();
-    process.env = { ...PRE_TEST_ENV };
-    process.env.NODE_ENV = 'production';
+      expect(wrapped).toMatchSnapshot();
+      expect(wrapped.containsMatchingElement(<span>Then</span>)).toBe(false);
+      expect(wrapped.html()).toBe('');
+    });
+
+    test('GIVEN <Then /> & <Else /> THEN renders children of <Else />', () => {
+      const wrapped = shallow(
+        <If condition={false}>
+          <Then>
+            <span>Then</span>
+          </Then>
+          <Else>
+            <span>Else</span>
+          </Else>
+        </If>
+      );
+
+      expect(wrapped).toMatchSnapshot();
+      expect(wrapped.containsMatchingElement(<span>Then</span>)).toBe(false);
+      expect(wrapped.containsMatchingElement(<span>Else</span>)).toBe(true);
+    });
+
+    test('GIVEN multiple <Else /> THEN renders only first <Else /> block', () => {
+      const wrapped = shallow(
+        <If condition={false}>
+          <Else>
+            <span>Else1</span>
+          </Else>
+          <Else>
+            <span>Else2</span>
+          </Else>
+        </If>
+      );
+
+      expect(wrapped).toMatchSnapshot();
+      expect(wrapped.containsMatchingElement(<span>Else1</span>)).toBe(true);
+      expect(wrapped.containsMatchingElement(<span>Else2</span>)).toBe(false);
+    });
+
+    test('GIVEN multiple <Else /> before <Then /> THEN renders <Else /> block', () => {
+      const wrapped = shallow(
+        <If condition={false}>
+          <Else>
+            <span>Else</span>
+          </Else>
+          <Then>
+            <span>Then</span>
+          </Then>
+        </If>
+      );
+
+      expect(wrapped).toMatchSnapshot();
+      expect(wrapped.containsMatchingElement(<span>Else</span>)).toBe(true);
+      expect(wrapped.containsMatchingElement(<span>Then</span>)).toBe(false);
+    });
+    test('GIVEN w/o <Then /> nor <Else /> THEN throws error', () => {
+      expect(() =>
+        shallow(
+          <If condition={false}>
+            <span>Content</span>
+          </If>
+        )
+      ).toThrowError('The <If> component should contain <Then /> and <Else /> components as its children');
+    });
+
+    test('GIVEN w/o children THEN renders null', () => {
+      const wrapped = shallow(<If condition={false} />);
+
+      expect(wrapped).toMatchSnapshot();
+      expect(wrapped.html()).toBeNull();
+    });
   });
 
-  afterAll(() => {
-    process.env = PRE_TEST_ENV;
+  describe('With NODE_ENV === production', () => {
+    beforeEach(() => {
+      // @ts-expect-error __DEV__ is exposed by Jest
+      global.__DEV__ = false;
+    });
+
+    afterAll(() => {
+      // @ts-expect-error __DEV__ is exposed by Jest
+      global.__DEV__ = true;
+    });
+
+    test('GIVEN content w/o <Then /> nor <Else /> THEN renders empty block in production', () => {
+      const wrapped = shallow(
+        <If condition={false}>
+          <span>Not Then nor Else</span>
+        </If>
+      );
+
+      expect(wrapped).toMatchSnapshot();
+      expect(wrapped.containsMatchingElement(<span>Not Then nor Else</span>)).toBe(false);
+    });
   });
 
-  test('GIVEN content w/o <Then /> nor <Else /> THEN renders empty block in production', () => {
-    const wrapped = shallow(
-      <If condition={true}>
-        <span>Not Then nor Else</span>
-      </If>
-    );
+  describe('With child as function', () => {
+    test('GIVEN <Else /> THEN renders children returned by function', () => {
+      const wrapped = mount(
+        <If condition={false}>
+          <Else>{() => <span>Else</span>}</Else>
+        </If>
+      );
 
-    expect(wrapped).toMatchSnapshot();
-    expect(wrapped.containsMatchingElement(<span>Not Then nor Else</span>)).toBe(true);
+      expect(wrapped).toMatchSnapshot();
+      expect(wrapped.containsMatchingElement(<span>Else</span>)).toBe(true);
+    });
+
+    test('GIVEN body that should not evaluate THEN should not render', () => {
+      // @ts-expect-error called should just be declared
+      let called = false;
+
+      const wrapped = mount(
+        <If condition={false}>
+          <Then>
+            {() => {
+              // eslint-disable-next-line @typescript-eslint/no-unused-vars
+              called = true;
+              <div>Bad</div>;
+            }}
+          </Then>
+          <Else>
+            <div>Ok</div>
+          </Else>
+        </If>
+      );
+
+      expect(wrapped).toMatchSnapshot();
+      expect(wrapped.containsMatchingElement(<div>Ok</div>)).toBe(true);
+      expect(wrapped.containsMatchingElement(<div>Bad</div>)).toBe(false);
+    });
   });
 });
