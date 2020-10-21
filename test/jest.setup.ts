b88a7624de4e5e67f2ec7276d6dd58dc8bee6600
Jeroen Claassens <support@favware.tech>
2020-10-21 21:01:30 +0200

    chore: formatting and cleanup

--- test/jest.setup.ts
+++ test/jest.setup.ts
@@ -1,7 +1,7 @@
-import 'jest-enzyme';
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import { createSerializer } from 'enzyme-to-json';
+import 'jest-enzyme';
 
 configure({ adapter: new Adapter() });
 
