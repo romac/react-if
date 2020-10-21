8f063670000fa51b2afea65316769f376a2338ea
Jeroen Claassens <support@favware.tech>
2020-10-21 20:35:03 +0200

    test: unit testing for switch/case/default

--- src/Default.tsx
+++ src/Default.tsx
@@ -1,9 +1,8 @@
 import { FC } from 'react';
 import { render } from './render';
-import { ComponentWithConditionProps } from './types';
 
 /** If no `<Case />` have its condition evaluates to true inside the parent `<Switch />`, the first `<Default />` will be the only one rendered. */
-export const Default: FC<ComponentWithConditionProps> = props => render(props);
+export const Default: FC = props => render(props);
 
 Default.defaultProps = {
   children: null
