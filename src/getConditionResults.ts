0129003007ff78ea86b7d2b525bcfaf5ddc7ff66
Jeroen Claassens <support@favware.tech>
2020-10-26 15:36:26 +0100

    chore: update eslint dependencies

--- src/getConditionResults.ts
+++ src/getConditionResults.ts
@@ -4,7 +4,7 @@ import { BooleanLike } from './types';
  * Resolves a condition that is {@link BooleanLike} or returns {@link BooleanLike} from a function
  * @param condition The condition to resolve
  */
-export const getConditionResult = (condition: BooleanLike | ((...args: unknown[]) => BooleanLike)) => {
+export const getConditionResult = (condition: BooleanLike | ((...args: unknown[]) => BooleanLike)): boolean => {
   const conditionResult = Boolean(typeof condition === 'function' ? condition() : condition);
 
   return conditionResult;
