3964c9547efe679fcb9f83ee9224dc0601e8dd60
iwfan <i.wangfancn@gmail.com>
2023-06-27 22:01:06 +0800

    fix: ts-jest configuration warn when run test
    (WARN) Define `ts-jest` config under `globals` is deprecated

--- jest.config.js
+++ jest.config.js
@@ -4,10 +4,15 @@ const config = {
   testMatch: ['<rootDir>/test/**/*.test.(ts|tsx)'],
   setupFilesAfterEnv: ['<rootDir>/test/jest.setup.ts'],
   globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/test/tsconfig.json'
-    },
     __DEV__: true
+  },
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/test/tsconfig.json'
+      }
+    ]
   },
   coveragePathIgnorePatterns: ['<rootDir>/src/Augments.d.ts', '<rootDir>/src/tinyWarning.ts', '<rootDir>/src/isThenable.ts']
 };
