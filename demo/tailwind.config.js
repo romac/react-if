7735ab7437c66f1d7d1bd4b6db9f477e2c8aa2d6
Jeroen Claassens <support@favware.tech>
2023-11-19 12:29:30 +0100

    chore: update dependencies

--- demo/tailwind.config.js
+++ demo/tailwind.config.js
@@ -18,16 +18,12 @@ module.exports = {
       {
         custom: {
           primary: '#5865f2',
-          'primary-focus': '#313ef2',
           'primary-content': '#ffffff',
           secondary: '#ed4245',
-          'secondary-focus': '#f61e21',
           'secondary-content': '#000000',
           accent: '#57f287',
-          'accent-focus': '#0bea52',
           'accent-content': '#000000',
           neutral: '#1f2123',
-          'neutral-focus': '#0e0f10',
           'neutral-content': '#ffffff',
           'base-100': '#2c2f33',
           'base-200': '#212123',
